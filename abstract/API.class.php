beaf719163df3e1e537d9e57b78a06fe9db26bc5
theredfoxfire <vizzlearn@gmail.com>
2014-11-07 20:46:26 +0700

    completing API.class

--- abstract/API.class.php
+++ abstract/API.class.php
@@ -50,4 +50,40 @@ abstract class API{
 				break;
 		}
 	}
+	
+	public function processAPI(){
+		if ((int)method_exists($this, $this->endpoint) > 0){
+			return $this->_response($this->{$this->endpoint}($this->args));
+		}
+		return $this->_response("No Endpoint: $this->endpoint", 404);
+	}
+	
+	private function _response($data, $status = 200){
+		header("HTTP/1.1".$status." ".$this->_requestStatus($status));
+		return json_encode($data);
+	}
+	
+	private function _cleanInputs($data){
+		$clean_input = Array();
+		if(is_array($data)){
+			foreach($data as $k => $v){
+				$clean_input[$k] = $this->_cleanInputs($v);
+			}
+		}else{
+			$clean_input = trim(strip_tags($data));
+		}
+		
+		return $clean_input;
+	}
+	
+	private function _requestStatus($code){
+		$status = array(
+			200 => 'OK',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			500 => 'Internal Server Error',
+		);
+		
+		return ($status[$code])?$status[$code]:$status[500];
+	}
 }
